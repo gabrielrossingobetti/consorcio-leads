d19ce8ac5246822a13b667709546d66b00435686
Gabriel <gabrielrossingobetti@gmail.com>
2026-05-29 12:22:32 -0300

    Add Microsoft Clarity tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,6 +41,15 @@ export default function RootLayout({
             gtag('config', 'G-4EJBKTY48M');
           `}
         </Script>
+        <Script id="microsoft-clarity" strategy="afterInteractive">
+          {`
+            (function(c,l,a,r,i,t,y){
+              c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+              t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+              y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "wyquou728a");
+          `}
+        </Script>
       </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
