5e6e1a10d9e75ad4213c4bd848d0c40a67f7c555
gabrielrossingobetti <gabrielrossingobetti@gmail.com>
2026-06-12 14:04:12 -0300

    rebrand: logo Indica Consórcio, remove Ademicon

--- components/AdemIconLogo.tsx
+++ components/AdemIconLogo.tsx
@@ -4,17 +4,15 @@ export default function AdemIconLogo({ size = 'md' }: { size?: 'sm' | 'md' | 'lg
 
   return (
     <div className="flex items-center gap-2.5">
-      {/* eslint-disable-next-line @next/next/no-img-element */}
-      <img
-        src="/ademicon-icon.png"
-        alt="Ademicon"
-        width={Math.round(40 * s)}
-        height={Math.round(40 * s)}
-        style={{ objectFit: 'contain' }}
-      />
+      <div
+        className="bg-red-600 rounded-lg flex items-center justify-center font-black text-white"
+        style={{ width: Math.round(40 * s), height: Math.round(40 * s), fontSize: Math.round(18 * s) }}
+      >
+        IC
+      </div>
       <div className="flex flex-col leading-none">
-        <span style={{ fontSize: Math.round(16 * s) }} className="font-black text-gray-900 tracking-wide">ADEMICON</span>
-        <span style={{ fontSize: Math.round(9 * s) }} className="text-gray-500 tracking-widest uppercase">administradora</span>
+        <span style={{ fontSize: Math.round(16 * s) }} className="font-black text-gray-900 tracking-wide">INDICA</span>
+        <span style={{ fontSize: Math.round(9 * s) }} className="text-gray-500 tracking-widest uppercase">consórcio</span>
       </div>
     </div>
   )
